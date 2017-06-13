ef75a1bc1d563c00fb5e887ea2ffa08d0a9382f7
Tri-C Faculty-Staff <Tri-C Faculty-Staff@LT5CG6250Q8R>
2017-06-13 14:57:37 -0400

    functions

---
+++ day 2 list/day 2 list/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_2_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<string> students = new List<string>();
+            students.Add("John");
+            students.Add("Thomas");
+            students.Add("joseph");
+            students.Add("Jacob");
+            students.Add("Parker");
+
+            for (int i = 0; i < students.Count; i++ )
+            {
+                Console.WriteLine(students[i]);
+
+                foreach (string student in students) ;
+                {
+                    Console.WriteLine(students);
+                }
+            }
+        }
+    }
+}
---
+++ day 2 project functions/day 2 project functions/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day_2_project_functions
+{
+
+    class Program
+    {
+        static string createnamestring(string firstname, string lastname)
+        {
+            string fullname = firstname + " " + lastname;
+            return fullname;
+        }
+
+        static void givestudentgrade(string firstname, string lastname, int score)
+        {
+            string grade = "A";
+            if (score > 79 && score < 90)
+            {
+                grade = "B";
+            }
+            else if (score > 69 && score < 80)
+            {
+                grade = "C";
+            }
+            else if (score > 59 && score < 70)
+            {
+                grade = "D";
+            }
+            else if (score > -1 && score < 60)
+            {
+                grade = "F";
+            }
+            Console.WriteLine(createnamestring(firstname, lastname) + ": " + grade);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(createnamestring("Iona", "Ford"));
+            givestudentgrade("Tim", "Burr", 30);
+            givestudentgrade("Al", "Dente", 99);
+            givestudentgrade("Sue", "Flay", 91);
+        }
+    }
+}
+
